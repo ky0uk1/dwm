eb001631d1f40222bf3f7cba350f779c1208c80d
ky0uk1 <gabrielraraujo456@gmail.com>
2023-08-05 14:00:26 -0300

    Changes to window border, hotkeys, and implementation of changing views with arrow keys

---
+++ shiftview.c
@@ -0,0 +1,20 @@
+/** Function to shift the current view left or right
+ *
+ *  @param "arg->i" stores the number of tags to shift right (positive)
+ *  or left (negative)
+ *
+ */
+
+void shiftview(const Arg *arg) {
+	Arg shifted;
+
+	if(arg->i > 0) //left circular shift
+		shifted.ui = selmon->tagset[selmon->seltags] << arg->i
+			| selmon->tagset[selmon->seltags] >> (LENGTH(tags) - arg->i);
+	else //right circular shift
+		shifted.ui = selmon->tagset[selmon->seltags] >> (- arg->i)
+			| selmon->tagset[selmon->seltags] << (LENGTH(tags) + arg->i);
+
+	view(&shifted);
+
+}
